fcc5479c91eaa19bfaa69e0c858bf6ffad291bed
hory01 <hor01@live.com>
2019-03-12 11:25:51 +0100

    Title link & styling

--- StrategyGameClient/src/app/services/heroes.service.ts
+++ StrategyGameClient/src/app/services/heroes.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Hero } from '../models/hero';
-import { Observable } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
